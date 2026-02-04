a4345dbe94ad655532c9e7889527c56862795c79
조혜린 <hyer1205@gmail.com>
2026-02-05 00:06:03 +0900

    fix: header border 안보이는 문제 해결

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,14 +4,13 @@ function Header() {
     const location = useLocation();
 
     const getLinkColor = (path: string) => {
-        console.log(path === location.pathname)
         return location.pathname == path 
             ? "text-foundation-blue-9" 
             : "text-foundation-black-text";
     };
 
     return (
-        <header className="w-full border-b border-foundation-gray-2 bg-white">
+        <header className="w-full border-b border-foundation-gray-5 bg-white">
             <nav className="mx-auto flex items-center px-60 h-20 relative">
                 <div className="bg-linear-to-r from-[#42C2FF] to-foundation-blue-9 bg-clip-text text-3xl font-extrabold font-pretendard text-transparent">
                     simvex
